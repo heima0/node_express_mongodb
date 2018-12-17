8c49a9e057d13173d30a31a89d4c8caf18c1055b
hanjinbao <hanjinbao@huanqiu.com>
2018-12-17 14:31:25 +0800

    node

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'node_express_mongodb',
+    script: './bin/www',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ali',
+      host : '47.75.210.58',
+      ref  : 'origin/master',
+      repo : 'git@github.com:heima0/node_express_mongodb.git',
+      path : '/home/node/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
