2e72a7a56dee55c918e1047647cbe23e22b2567c
hanjinbao <hanjinbao@huanqiu.com>
2018-12-20 12:40:53 +0800

    d

--- routes/connect/index.js
+++ routes/connect/index.js
@@ -3,13 +3,15 @@ const assert = require('assert');
 
 const connect = function(callback){
   // Connection URL
-  const url = 'http://0.0.0.0:27017/';
-   
+  // const url = 'http://0.0.0.0:27017/';
+  
   // Database Name
+  const url = 'mongodb://localhost:27017/';
   const dbName = '1726a';
-   
+  const fullUrl = url + dbName; // which should evaluate to this 'mongodb://localhost:27017/ceramo'
+
   // Use connect method to connect to the server
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(fullUrl, function(err, client) {
     assert.equal(null, err);
     console.log("数据库链接成功");
    
